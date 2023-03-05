ee196defb54a763a626c97b2ff410114f41ee80f
padman-1 <frimpongy890@gmail.com>
2023-03-05 10:37:53 -0500

    Created home page

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:fortune_room_booking_app/config/constants.dart';
+import 'package:salomon_bottom_bar/salomon_bottom_bar.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,6 +12,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  var _currentIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,6 +27,36 @@ class _HomePageState extends State<HomePage> {
             backgroundImage: AssetImage(
               'assets/images/mid2.jpg',
             ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: SalomonBottomBar(
+        currentIndex: _currentIndex,
+        onTap: (i) => setState(() => _currentIndex = i),
+        items: [
+          SalomonBottomBarItem(
+            icon: const Icon(Icons.home),
+            title: const Text('Home'),
+            selectedColor: secondaryColor,
+            unselectedColor: primaryColor,
+          ),
+          SalomonBottomBarItem(
+            icon: const Icon(Icons.menu_book),
+            title: Text('booking'),
+            selectedColor: secondaryColor,
+            unselectedColor: primaryColor,
+          ),
+          SalomonBottomBarItem(
+            icon: const Icon(Icons.history),
+            title: const Text('my bookings'),
+            selectedColor: secondaryColor,
+            unselectedColor: primaryColor,
+          ),
+          SalomonBottomBarItem(
+            icon: const Icon(Icons.settings),
+            title: Text('Settings'),
+            selectedColor: secondaryColor,
+            unselectedColor: primaryColor,
           ),
         ],
       ),
