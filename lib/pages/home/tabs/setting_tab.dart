cb6b069fd3a8d1d60921688e8a077a6e5e99d15c
padman-1 <frimpongy890@gmail.com>
2023-03-05 12:15:48 -0500

    added settings tab page

---
+++ lib/pages/home/tabs/setting_tab.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class SettingsTab extends StatelessWidget {
+  const SettingsTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text('SettingsTab'),
+      ),
+    );
+  }
+}
