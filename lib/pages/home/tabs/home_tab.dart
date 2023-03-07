b10be3ea523d80042e369dc9f61452527fd611f2
padman-1 <frimpongy890@gmail.com>
2023-03-07 01:08:30 -0500

    updated home tab page

--- lib/pages/home/tabs/home_tab.dart
+++ lib/pages/home/tabs/home_tab.dart
@@ -4,6 +4,8 @@ import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 
+import '../../../config/constants.dart';
+
 class HomeTab extends StatefulWidget {
   const HomeTab({super.key});
 
@@ -13,9 +15,9 @@ class HomeTab extends StatefulWidget {
 
 class _HomeTabState extends State<HomeTab> {
   final images = [
-    'assets/images/front3.jpg',
+    'assets/images/front1.jpg',
     'assets/images/mid1.jpg',
-    'assets/images/mid3.jpg',
+    'assets/images/mid2.jpg',
     'assets/images/room.jpg',
     'assets/images/welc2.jpg',
     'assets/images/tel3.jpg',
@@ -24,6 +26,7 @@ class _HomeTabState extends State<HomeTab> {
   Widget build(BuildContext context) {
     final devSize = MediaQuery.of(context).size;
     return SingleChildScrollView(
+      physics: const BouncingScrollPhysics(),
       child: Container(
           padding: const EdgeInsets.only(
             left: 10,
@@ -55,11 +58,169 @@ class _HomeTabState extends State<HomeTab> {
                   return buildImage(urlImage, index);
                 }),
                 options: CarouselOptions(
-                  height: 300,
+                  height: devSize.height / 2.5,
                   autoPlay: true,
-                  enlargeCenterPage: true,
+                  // enlargeCenterPage: true,
                   // autoPlayInterval: Duration(seconds: 0),
                 ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Center(
+                child: Text(
+                  'Take a look at our various facilities',
+                  style: GoogleFonts.manrope(fontSize: 18),
+                ),
+              ),
+              Column(
+                children: [
+                  Card(
+                    color: secondaryColor,
+                    elevation: 20,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 20,
+                        vertical: 10.0,
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Icon(
+                            Icons.apartment,
+                            color: Colors.white,
+                          ),
+                          Text(
+                            'Main Building',
+                            style: GoogleFonts.manrope(
+                              color: Colors.white,
+                            ),
+                          ),
+                          const Icon(
+                            Icons.arrow_right,
+                            color: Colors.white,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Card(
+                    color: primaryColor,
+                    elevation: 20,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 20,
+                        vertical: 10.0,
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Icon(
+                            Icons.hotel,
+                            color: Colors.white,
+                          ),
+                          Text(
+                            'Rooms',
+                            style: GoogleFonts.manrope(
+                              color: Colors.white,
+                            ),
+                          ),
+                          const Icon(
+                            Icons.arrow_right,
+                            color: Colors.white,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Card(
+                    color: secondaryColor,
+                    elevation: 20,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 20,
+                        vertical: 10.0,
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Icon(
+                            Icons.kitchen,
+                            color: Colors.white,
+                          ),
+                          Text(
+                            'Kitchen',
+                            style: GoogleFonts.manrope(
+                              color: Colors.white,
+                            ),
+                          ),
+                          const Icon(
+                            Icons.arrow_right,
+                            color: Colors.white,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Card(
+                    color: primaryColor,
+                    elevation: 20,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 20,
+                        vertical: 10.0,
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Icon(
+                            Icons.menu_book,
+                            color: Colors.white,
+                          ),
+                          Text(
+                            'Study Room',
+                            style: GoogleFonts.manrope(
+                              color: Colors.white,
+                            ),
+                          ),
+                          const Icon(
+                            Icons.arrow_right,
+                            color: Colors.white,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Card(
+                    color: secondaryColor,
+                    elevation: 20,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 20,
+                        vertical: 10.0,
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Icon(
+                            Icons.tv,
+                            color: Colors.white,
+                          ),
+                          Text(
+                            'TV Room',
+                            style: GoogleFonts.manrope(
+                              color: Colors.white,
+                            ),
+                          ),
+                          const Icon(
+                            Icons.arrow_right,
+                            color: Colors.white,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ],
           )),
@@ -67,10 +228,12 @@ class _HomeTabState extends State<HomeTab> {
   }
 
   Widget buildImage(String urlImage, int index) {
+    final devSize = MediaQuery.of(context).size;
     return Container(
-      // margin: const EdgeInsets.symmetric(
-      //   horizontal: 2,
-      // ),
+      width: devSize.width / 1.2,
+      margin: const EdgeInsets.symmetric(
+        horizontal: 12,
+      ),
       // color: Colors.grey,
       child: Image(
         image: AssetImage(urlImage),
