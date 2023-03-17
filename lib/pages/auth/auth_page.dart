4230b8decf155df3b6eac93ae8dccdbe67b97181
padman-1 <frimpongy890@gmail.com>
2023-03-17 01:15:06 -0400

    created auth page

---
+++ lib/pages/auth/auth_page.dart
@@ -0,0 +1,26 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:fortune_room_booking_app/pages/auth/sign_in_page.dart';
+import 'package:fortune_room_booking_app/pages/home/home_page.dart';
+
+class AuthPage extends StatelessWidget {
+  const AuthPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return HomePage();
+          } else {
+            return SignInPage();
+          }
+        },
+      ),
+    );
+  }
+}
