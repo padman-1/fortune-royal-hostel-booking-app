e655b9723d7b6d37297a7cb878e75fa60e02c8bf
padman-1 <frimpongy890@gmail.com>
2023-04-24 11:40:02 -0400

    floor rooms page modified

--- lib/pages/home/floors/floor_rooms.dart
+++ lib/pages/home/floors/floor_rooms.dart
@@ -3,21 +3,24 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:fortune_room_booking_app/config/constants.dart';
+import 'package:fortune_room_booking_app/pages/home/rooms/ac_rooms.dart';
+import 'package:fortune_room_booking_app/pages/home/tabs/booking_tab.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import '../../../models/floor_model.dart';
 
-class Rooms extends StatefulWidget {
-  const Rooms({
+class RoomType extends StatefulWidget {
+  const RoomType({
     super.key,
+    required this.floormodel,
   });
-  // final Floor floormodel;
+  final Floor floormodel;
 
   @override
-  State<Rooms> createState() => _RoomsState();
+  State<RoomType> createState() => _RoomTypeState();
 }
 
-class _RoomsState extends State<Rooms> {
+class _RoomTypeState extends State<RoomType> {
   @override
   Widget build(BuildContext context) {
     final devSize = MediaQuery.of(context).size;
@@ -25,10 +28,11 @@ class _RoomsState extends State<Rooms> {
       appBar: AppBar(
         title: Text('Book Room'),
         centerTitle: true,
-        backgroundColor: Colors.grey[200],
+        backgroundColor: backgroundColor,
         foregroundColor: Colors.black54,
       ),
       body: Container(
+        color: backgroundColor,
         height: devSize.height,
         padding: const EdgeInsets.symmetric(
           horizontal: 20,
@@ -46,34 +50,42 @@ class _RoomsState extends State<Rooms> {
             const SizedBox(
               height: 10,
             ),
-            Container(
-              height: devSize.height / 3,
-              child: GridView.builder(
-                itemCount: 20,
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 4,
-                  mainAxisSpacing: 10,
-                  crossAxisSpacing: 10,
+            GestureDetector(
+              onTap: () => Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const ACRooms(),
                 ),
-                itemBuilder: (context, int index) {
-                  return Container(
-                    // height: 80,
-                    // width: 80,
-                    decoration: BoxDecoration(
-                      border: Border.all(),
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: const [
-                        Text(
-                          'Room',
-                        ),
-                        Icon(Icons.hotel),
-                      ],
-                    ),
-                  );
-                },
+              ),
+              child: Container(
+                height: devSize.height / 3,
+                child: GridView.builder(
+                  itemCount: widget.floormodel.roomsWithAc,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 4,
+                    mainAxisSpacing: 10,
+                    crossAxisSpacing: 10,
+                  ),
+                  itemBuilder: (context, int index) {
+                    return Container(
+                      // height: 80,
+                      // width: 80,
+                      decoration: BoxDecoration(
+                        color: primaryColor.withOpacity(0.2),
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Room ${index + 1}',
+                          ),
+                          Icon(Icons.hotel),
+                        ],
+                      ),
+                    );
+                  },
+                ),
               ),
             ),
             const SizedBox(
@@ -91,28 +103,30 @@ class _RoomsState extends State<Rooms> {
             Container(
               height: devSize.height / 3,
               child: GridView.builder(
-                itemCount: 20,
+                itemCount: widget.floormodel.roomsWithoutAC,
                 gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                   crossAxisCount: 4,
                   mainAxisSpacing: 10,
                   crossAxisSpacing: 10,
                 ),
                 itemBuilder: (context, int index) {
-                  return Container(
-                    // height: 80,
-                    // width: 80,
-                    decoration: BoxDecoration(
-                      border: Border.all(),
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: const [
-                        Text(
-                          'Room',
-                        ),
-                        Icon(Icons.hotel),
-                      ],
+                  return GestureDetector(
+                    child: Container(
+                      // height: 80,
+                      // width: 80,
+                      decoration: BoxDecoration(
+                        color: secondaryColor.withOpacity(0.2),
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Room ${index + widget.floormodel.roomsWithoutAC + 1}',
+                          ),
+                          Icon(Icons.hotel),
+                        ],
+                      ),
                     ),
                   );
                 },
