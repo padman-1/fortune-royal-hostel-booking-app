b0c2adebc79f6d87ea0dfca95af091884a9f137d
padman-1 <frimpongy890@gmail.com>
2023-04-24 11:36:04 -0400

    Floor model updated

--- lib/models/floor_model.dart
+++ lib/models/floor_model.dart
@@ -1,9 +1,11 @@
 class Floor {
   final int roomsWithAc;
   final int roomsWithoutAC;
+  final String floorname;
 
   Floor({
     required this.roomsWithAc,
     required this.roomsWithoutAC,
+    required this.floorname,
   });
 }
